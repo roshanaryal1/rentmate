021896116301ae2dd6c64bbbb231b7e4fe378de4
roshanaryal1 <roshanaryaal@gmail.com>
2025-05-16 11:40:04 +1200

    basic admin dashbord structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';  // Add this line
+import 'bootstrap-icons/font/bootstrap-icons.css';  // Add this line for icons
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 console.log('index.js loading...');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 console.log('Root element found:', document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 console.log('App rendered!');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
