a88a3d52996669020e2cfaafded5066a78205ea3
roshanaryal1 <roshanaryaal@gmail.com>
2025-06-10 13:26:19 +1200

    added tos/pp on signup

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { TermsModal, PrivacyModal } from './LegalModals';
 
 export default function Signup() {
   // Form data state
